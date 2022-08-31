6693c672473b563ccc57757025cc26eacf232f31
unknown <huynghials123@gmail.com>
2022-08-31 11:50:47 +0700

    config nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/LuckyCircle/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-test',
